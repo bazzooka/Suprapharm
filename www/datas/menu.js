8593a1687a06883df4ad5d198e02d900397d5fae
Joe <bazzooka@gmail.com>
2015-06-17 18:39:41 +0200

    add datas/menus

---
+++ www/datas/menu.js
@@ -0,0 +1,26 @@
+export var Menus = {
+    entries: [
+        {
+            "title" : "Accueil",
+            "href": "#/"
+        }, {
+            "title" : "Catalogue",
+            "href": "#/catalogue"
+        }, {
+            "title" : "Pharmacies",
+            "href": "#/pharmacies"
+        }, {
+            "title" : "Conseils",
+            "href": "#/conseils"
+        }, {
+            "title" : "Jeux",
+            "href": "#/jeux"
+        }, {
+            "title" : "Jeux",
+            "href": "#/jeux"
+        }, {
+            "title" : "M&eacute;dicaments",
+            "href": "#/medicaments"
+        }
+    ]
+};
